f047a2929bebe1628baeb8264ada8f27b297395f
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-07-23 16:00:23 +0530

    goroutines

--- tutorial/cmd/main.go
+++ tutorial/cmd/main.go
@@ -1,12 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// var m = sync.RWMutex{}
+// var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
+// var results = []string{}
+var wg = sync.WaitGroup{}
 
 func main() {
-	intArray := [...]uint8{128, 135, 119, 226, 255, 250}
-	fmt.Println(intArray)
-
-	fmt.Println(&intArray[0])
-	fmt.Println(&intArray[1])
-	fmt.Println(&intArray[2])
+	t0 := time.Now()
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go count()
+		// go dbCall()
+	}
+	wg.Wait()
+	fmt.Printf("\nTotal Execution Time: %v\n", time.Since(t0))
+	// fmt.Println("The results are ", results)
 }
+
+func count() {
+	var res int
+	for i := 0; i < 100000000; i++ {
+		res++
+	}
+	wg.Done()
+}
+
+// func dbCall() {
+// 	var delay float32 = 2000
+// 	time.Sleep(time.Duration(delay) * time.Millisecond)
+// 	save(dbData[i])
+// 	log()
+// 	wg.Done()
+// }
+
+// func save(result string) {
+// 	m.Lock()
+// 	results = append(results, result)
+// 	m.Unlock()
+// }
+
+// func log() {
+// 	m.RLock()
+// 	fmt.Printf("\nThe current results are: %v\n", results)
+// 	m.RUnlock()
+// }
--- tutorial/cmd/side.go
+++ tutorial/cmd/side.go
@@ -35,3 +35,97 @@ func intDivision(numerator int, denominator int) (int, int, error) {
 	var remainder int = numerator % denominator
 	return quotient, remainder, myError
 }
+
+// intArray := [...]uint8{128, 135, 119, 226, 255, 250}
+// 	fmt.Println(intArray)
+
+// 	fmt.Println(&intArray[0])
+// 	fmt.Println(&intArray[1])
+// 	fmt.Println(&intArray[2])
+
+// var myString = []rune("Résumé")
+// 	var indexed = myString[2]
+// 	fmt.Printf("%v, %T\n", indexed, indexed)
+// 	for i, v := range myString {
+// 		fmt.Println(i, v)
+// 	}
+// 	fmt.Printf("\nThe length of myString is %v\n", len(myString))
+
+// 	var myRune = 'a'
+// 	fmt.Printf("\nmyRune = %v, Type = %T\n", myRune, myRune)
+
+// 	var strSlice = []string{"s", "u", "s"}
+// 	var strBuilder strings.Builder
+// 	for i := range strSlice {
+// 		strBuilder.WriteString(strSlice[i])
+// 	}
+// 	var catString = strBuilder.String()
+// 	fmt.Printf("\n%v\n", catString)
+
+// type gasEngine struct {
+// 	gallons uint8
+// 	mpg     uint8
+// 	owner
+// }
+// type electricEngine struct {
+// 	mpkwh uint8
+// 	kwh   uint8
+// 	owner
+// }
+
+// type owner struct {
+// 	name string
+// }
+
+// func (e gasEngine) milesLeft() uint8 {
+// 	return (e.gallons * e.mpg)
+// }
+
+// func (e electricEngine) milesLeft() uint8 {
+// 	return (e.kwh * e.mpkwh)
+// }
+
+// func canMakeIt(e engine, miles uint8) {
+// 	if miles <= e.milesLeft() {
+// 		fmt.Println("You can make it!")
+// 	} else {
+// 		fmt.Println("Fuel up. Hurry!")
+// 	}
+// }
+
+// // checks if whatever passed has a milesLeft() method with following signature
+// type engine interface {
+// 	milesLeft() uint8
+// }
+
+// var newEngine = gasEngine{8, 20, owner{"Darsh"}}
+// 	fmt.Println(newEngine.name, newEngine.milesLeft())
+// 	canMakeIt(newEngine, 150)
+// 	canMakeIt(electricEngine{8, 12, owner{"Darsh"}}, 80)
+
+// var p *int32 = new(int32)
+// 	var i int32 = 9
+// 	fmt.Printf("The value at *p is %v\n", *p)
+// 	fmt.Printf("The value at i is %v\n", i)
+// 	*p = 10
+// 	fmt.Printf("The value at *p is %v\n", *p)
+// 	p = &i
+// 	fmt.Printf("The value at *p is %v\n", *p)
+// 	*p = 3
+// 	fmt.Printf("The value at i is %v\n", i)
+// 	fmt.Printf("The value at *p is %v\n", *p)
+
+// var thing1 = [5]float64{1, 2, 3, 4, 5}
+// fmt.Printf("Memory Location of thing1: %p\n", &thing1)
+// var result [5]float64 = square(&thing1)
+// fmt.Printf("The result is: %v\n", result)
+// fmt.Println("The value of thing1 is ", thing1)
+
+// func square(thing2 *[5]float64) [5]float64 {
+// 	fmt.Printf("The memory location of thing2: %p\n", thing2)
+// 	fmt.Printf("The value of thing2: %v\n", *thing2)
+// 	for i := range *thing2 {
+// 		thing2[i] = thing2[i] * thing2[i]
+// 	}
+// 	return *thing2
+// }
